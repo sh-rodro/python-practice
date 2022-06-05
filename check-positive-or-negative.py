1528a27ef520f03c718a1204030ef9cb784f72a5
Sakhawat Hossain Rodro <sakhawatrodro@gmail.com>
2022-06-05 17:42:47 +0600

    Create check-positive-or-negative.py

---
+++ check-positive-or-negative.py
@@ -0,0 +1,11 @@
+#python program to check if a number is Positive or Negative
+
+num = input("Please type an integer: ") 
+num = int(num) 
+
+if num < 0: 
+  result = "Negative" 
+else: 
+  result = "Positive" 
+
+print("This number is: ", result)
