677ecfc8efcc48c8acf63ca0b652efe02826fa65
Sakhawat Hossain Rodro <sakhawatrodro@gmail.com>
2022-06-06 21:40:00 +0600

    Update check-leap-year.py

--- check-leap-year.py
+++ check-leap-year.py
@@ -4,12 +4,12 @@ year = input("Enter year: ")
 year = int(year) 
 
 if year % 400 == 0: 
-  result = "a leap year" 
+    result = "a leap year" 
 elif year % 100 == 0: 
-  result = "not a leap year" 
+    result = "not a leap year" 
 elif year % 4 == 0: 
-  result = "a leap year" 
+    result = "a leap year" 
 else: 
-  result = "not a leap year" 
+    result = "not a leap year" 
   
 print("This year is: ", result)
