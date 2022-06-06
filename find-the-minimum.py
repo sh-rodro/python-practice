b47c65cd5087bc590f6f588e5c7b9b8cf8cde368
Sakhawat Hossain Rodro <sakhawatrodro@gmail.com>
2022-06-06 22:56:16 +0600

    Update find-the-minimum.py

--- find-the-minimum.py
+++ find-the-minimum.py
@@ -9,11 +9,11 @@ num2 = int(num2)
 num3 = int(num3) 
 
 if num1 < num2: 
-  min = num1 
+    min = num1 
 else: 
-  min = num2 
+    min = num2 
 if num3 < min: 
-  min = num3 
+    min = num3 
 
 result = min 
 
