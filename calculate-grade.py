0ece0b9bfc33cf0b899b3deb76feb2c55f90bd13
Sakhawat Hossain Rodro <sakhawatrodro@gmail.com>
2022-06-06 21:36:23 +0600

    Update calculate-grade.py

--- calculate-grade.py
+++ calculate-grade.py
@@ -4,14 +4,14 @@ marks = input("Enter your marks: ")
 marks = int(marks) 
 
 if marks >= 80: 
-  grade = ("A+") 
+    grade = ("A+") 
 elif marks >= 70: 
-  grade = ("A") 
+    grade = ("A") 
 elif marks >= 60: 
-  grade = ("A-") 
+    grade = ("A-") 
 elif marks >= 50: 
-  grade = ("B") 
+    grade = ("B") 
 else: 
-  grade = ("F") 
+    grade = ("F") 
 
 print("Your grade is: ", grade) 
